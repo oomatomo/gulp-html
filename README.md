99ee8fbf18d942c02dfa12f33245c96e6f752d9d
oomatomo <ooma0301@gmail.com>
2017-08-12 00:30:33 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# gulp
+
+## 起動
+
+```
+yarn
+yarn gulp
+```
+
+## node_modulesのファイルを使う場合
+
+gulp-npm-filesを使って`dist/node_modules/..`にファイルを置けるようにしているので
+以下のように読み込める
+
+```
+<link rel="stylesheet" href="node_modules/swiper/dist/css/swiper.min.css">
+```
---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require("gulp");
+var ejs = require("gulp-ejs");
+var gutil = require('gulp-util');
+
+var $ = require('gulp-load-plugins')();
+
+var webserver = require('gulp-webserver');
+var prettify  = require('gulp-prettify');
+var htmlhint  = require("gulp-htmlhint");
+var minify    = require('gulp-minify');
+var rename    = require("gulp-rename");
+var plumber = require('gulp-plumber')
+var gnf = require('gulp-npm-files');
+
+gulp.task('webserver', function() {
+  gulp.src('dist/')
+    .pipe(webserver({
+      host: '127.0.0.1',
+      livereload: true,
+      open: true
+    }));
+});
+
+gulp.task('js', function () {
+  gulp.src('src/**/js/*.js')
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('html', function() {
+  gulp.src('src/**/*.html')
+    .pipe(prettify({indent_size: 2}))
+    .pipe(htmlhint())
+    .pipe(htmlhint.reporter())
+    .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('css', function () {
+  gulp.src(['src/**/css/*.css', 'src/**/css/**/**.css'])
+   .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('img', function() {
+  gulp.src('src/**/images/*')
+    .pipe(gulp.dest('dist/'));
+  gulp.src('src/**/images/*/*')
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('font', function() {
+  gulp.src('src/**/fonts/*')
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('lib', function() {
+  gulp.src(gnf(), {base:'./'})
+    .pipe(gulp.dest('dist/'));
+});
+
+//ファイルの監視
+gulp.task('watch',function(){
+  gulp.watch(['src/**/*.html'],['html']);
+  gulp.watch(['src/js/*.js', 'src/**/js/*.js'],['js']);
+  gulp.watch(['src/css/*', 'src/**/css/*', 'src/**/css/**/*'],['css']);
+  gulp.watch(['src/images/*', 'src/**/images/*'],['img']);
+  gulp.watch(['src/fonts/*', 'src/**/fonts/*'],['font']);
+});
+
+gulp.task('default',['js', 'html', 'css', 'img', 'font', 'webserver', 'watch', 'lib']);
